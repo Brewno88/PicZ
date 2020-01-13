df2f2a3e1517dcda1a52a87440b284c40cdb7633
Vincenzo <ilcontevince@gmail.com>
2020-01-13 21:15:15 +0000

    add setSpan()

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -10,14 +10,16 @@ class ImageCard extends React.Component {
   }
 
   componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
 
+  setSpans = () => {
     const height = this.imageRef.current.clientHeight;
 
     const spans = Math.ceil(height / 10);
 
     this.setState({ spans });
-
-}
+  };
 
   render() {
     const { description, urls } = this.props.image;
